749f4ef2fc3d86470f9cb1d9247535c37922662a
Mohamammed Faizal <mohammad.faizal@digital14.com>
2022-03-05 10:58:56 +0400

    face connections working

--- faceMesh_Minimum.py
+++ faceMesh_Minimum.py
@@ -4,6 +4,9 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh()
+
 mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
@@ -11,6 +14,15 @@ cTime = 0
 
 while True:
     success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+
+    #print(results)
+    #print(dir(results))
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS)
+
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
